902721b4ac570956c684b8b8b137b946de942b8e
ElicomElijah <125479282+Elijah-ug@users.noreply.github.com>
2025-10-15 12:27:36 +0300

    added artifacts to the frontend and  && contract deployment

--- tution-ui/src/contract/address/address.js
+++ tution-ui/src/contract/address/address.js
@@ -1,2 +1,2 @@
-export const contractAddress = "0x94f88E3388EF9b33329bf86a0E4c2721FC81609a";
+export const contractAddress = "0xFf6159cf2E88A03cb4238A5A7474BeF37F41E6b0";
 export const tokenAddr = "0xd2d8eb06e3f91f0c15257762b29cbb101518fed0";
