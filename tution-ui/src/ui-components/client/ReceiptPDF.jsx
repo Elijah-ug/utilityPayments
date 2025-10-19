ea6e24e32ef47295945be421bd1515bf1123d587
ElicomElijah <125479282+Elijah-ug@users.noreply.github.com>
2025-10-20 02:15:31 +0300

    added 3 cols in the tx table & redeployed the db

--- tution-ui/src/ui-components/client/ReceiptPDF.jsx
+++ tution-ui/src/ui-components/client/ReceiptPDF.jsx
@@ -1,8 +1,9 @@
 import { Document, Page, View, Text } from '@react-pdf/renderer';
 import { formatEther } from 'viem';
 import { styles } from './styles';
+import { hexToString } from 'viem';
 
-export default function ReceiptPDF({ receipt, date, hexToString, isPending }) {
+export default function ReceiptPDF({ receipt, date, isPending }) {
   console.log('isPending==>', isPending);
 
   <Document>
@@ -14,59 +15,59 @@ export default function ReceiptPDF({ receipt, date, hexToString, isPending }) {
           {/* Header */}
           <View style={styles.header}>
             <Text style={styles.title}>Payment Receipt</Text>
-            {/* <Text style={styles.subtitle}>Tuition Payment Confirmation</Text> */}
+            <Text style={styles.subtitle}>Tuition Payment Confirmation</Text>
           </View>
 
           {/* Divider */}
-          {/* <Text style={styles.divider} /> */}
+          <Text style={styles.divider} />
 
           {/* Content */}
           <View style={styles.section}>
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Payer Address:</Text>
-          <Text style={styles.value}>
-            {`${receipt.payer.slice(0, 7)}...${receipt?.payer.slice(-5)}`}
-          </Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Payer Address:</Text>
+              <Text style={styles.value}>
+                {`${receipt.payer.slice(0, 7)}...${receipt?.payer.slice(-5)}`}
+              </Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>School Address:</Text>
-          <Text style={styles.value}>
-            {`${receipt.school.slice(0, 7)}...${receipt?.school.slice(-5)}`}
-          </Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>School Address:</Text>
+              <Text style={styles.value}>
+                {`${receipt.school.slice(0, 7)}...${receipt?.school.slice(-5)}`}
+              </Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Student Name:</Text>
-          <Text style={styles.value}>{hexToString(receipt.student)}</Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Student Name:</Text>
+              <Text style={styles.value}>{hexToString(receipt.student)}</Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Class:</Text>
-          <Text style={styles.value}>{hexToString(receipt.class)}</Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Class:</Text>
+              <Text style={styles.value}>{hexToString(receipt.class)}</Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Amount Paid:</Text>
-          <Text style={styles.value}>{formatEther(receipt.amount)} AFB</Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Amount Paid:</Text>
+              <Text style={styles.value}>{formatEther(receipt.amount)} AFB</Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Remaining Balance:</Text>
-          <Text style={styles.value}>{formatEther(receipt.balance)} AFB</Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Remaining Balance:</Text>
+              <Text style={styles.value}>{formatEther(receipt.balance)} AFB</Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Payment Date:</Text>
-          <Text style={styles.value}>{date}</Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Payment Date:</Text>
+              <Text style={styles.value}>{date}</Text>
+            </View>
 
-            {/* <View style={styles.row}>
-          <Text style={styles.label}>Status:</Text>
-          <Text style={[styles.value, receipt.cleared ? styles.cleared : styles.pending]}>
-            {receipt.cleared ? 'Cleared' : 'Not cleared'}
-          </Text>
-        </View> */}
+            <View style={styles.row}>
+              <Text style={styles.label}>Status:</Text>
+              <Text style={[styles.value, receipt.cleared ? styles.cleared : styles.pending]}>
+                {receipt.cleared ? 'Cleared' : 'Not cleared'}
+              </Text>
+            </View>
           </View>
 
           {/* Footer */}
---
+++ tution-ui/src/ui-components/client/ReceiptView.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { formatEther, hexToString } from 'viem';
+
+export const ReceiptView = ({ receipt, date, hexToString, isPending }) => {
+  return (
+    <div className='text-blue-700'>
+      <h3>Wired here</h3>
+      {isPending ? (
+        <h3>Loading...</h3>
+      ) : (
+        receipt && (
+          <div className="flex flex-col gap-3">
+            <div className="">
+              <span>Payer</span>
+              <span>{`${receipt.payer.slice(0, 7)}...${receipt?.payer.slice(-5)}`}</span>
+            </div>
+
+            <div className="">
+              <span>Payer</span>
+              <span>{`${receipt.school.slice(0, 7)}...${receipt?.school.slice(-5)}`}</span>
+            </div>
+
+            <div className="">
+              <span>Student Name</span>
+              <span>{hexToString(receipt.student)}</span>
+            </div>
+
+            <div className="">
+              <span>Class:</span>
+              <span>{hexToString(receipt.class)}</span>
+            </div>
+
+            <div>
+              <span>Amount Paid:</span>
+              <span>{formatEther(receipt.amount)} AFB</span>
+            </div>
+
+            <div>
+              <span>Remaining Balance:</span>
+              <span>{formatEther(receipt.balance)} AFB</span>
+            </div>
+
+            <div>
+              <span>Payment Date:</span>
+              <span>{date}</span>
+            </div>
+
+            <div>
+              <span>Status:</span>
+              <span>{receipt.cleared ? 'Cleared' : 'Not cleared'}</span>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
